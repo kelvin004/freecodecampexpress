319a62d44b2c96089231fc73d1d5db844674608d
unknown <kelvinmbariya@gmail.com>
2019-11-10 15:19:31 +0530

    Freecodecamp express learning

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
-
+var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
+
 
 // --> 7)  Mount the Logger middleware here
 
@@ -9,43 +10,77 @@ var app = express();
 
 
 /** 1) Meet the node console. */
+console.log("Hello World")
 
+//To parse the body with encoded url
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+app.use(express.json())
+
+app.use(function middleware(req,res,next){
+  console.log(req.method + ' ' + req.path + ' - ' + req.ip)
+  next()
+})
 
 /** 2) A first working Express Server */
-
+app.get("/",function(req,res){
+  res.sendFile(__dirname + "/views/index.html")
+})
 
 /** 3) Serve an HTML file */
 
 
 /** 4) Serve static assets  */
-
+app.use('/', express.static(__dirname + "/public"))
 
 /** 5) serve JSON on a specific route */
-
+app.get("/json",function(req,res){
+  if(process.env.MESSAGE_STYLE=="uppercase")
+  {
+    res.json({"message":"HELLO JSON"})
+  }
+  else
+    {
+      res.json({"message":"Hello json"})
+    }
+})
 
 /** 6) Use the .env file to configure the app */
- 
+process.env.MESSAGE_STYLE="uppercase" 
  
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
 
 
 /** 8) Chaining middleware. A Time server */
-
+app.get("/now",function(req,res,next){
+  req.time = new Date().toString()
+  next()
+},function(req,res){
+  res.json({time:req.time})
+})
 
 /** 9)  Get input from client - Route parameters */
-
+app.get("/:word/echo",function(req,res){
+  res.json({echo:req.params.word})
+})
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
-
+/*
+app.get("/name",function(req,res){
+  res.json({name:req.query.first + ' ' + req.query.last})
+})*/
   
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
 
 
 /** 12) Get data form POST  */
-
+app.post("/name",function(req,res){
+  console.log(req.body)
+  res.json({"name" : req.body.first + ' ' + req.body.last})
+})
 
 
 // This would be part of the basic setup of an Express app
